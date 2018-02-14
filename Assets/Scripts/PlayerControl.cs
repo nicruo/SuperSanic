bb4a715ddbe4cf5bc6e3c42475706ff2cc839bad
José Medeiros <jmedeiros@agap2.pt>
2018-02-14 20:17:53 +0000

    Fixed hero position;

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour {
     
     private Rigidbody2D body;
+    private Animator anim;
+
 
     public float speed = 100;
     public float jumpSpeed = 20;
@@ -14,11 +16,14 @@ public class PlayerControl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         var horizontal = Input.GetAxis("Horizontal");
+
+        anim.SetBool("walking", true);
 
         body.AddForce(Vector2.right * horizontal * speed);
 
